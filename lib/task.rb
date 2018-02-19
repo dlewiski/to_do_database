24b89a2a5806ac6122399f48f5187289e9c391cb
David Lewis and Katie Fujihara <katie@>
2018-02-19 14:48:33 -0800

    Write spec to return date

--- lib/task.rb
+++ lib/task.rb
@@ -1,13 +1,29 @@
 class Task
-  attr_reader(:name)
+  attr_reader(:description, :due_date, :list_id)
 
-  def initialize(name)
-    @name = name
+  def initialize(attributes)
+    @description = attributes.fetch(:description)
+    @due_date = attributes.fetch(:due_date)
+    @list_id = attributes.fetch(:list_id)
   end
 
-  def ==(other)
-    same_class = self.class().eql?(other.class())
-    same_name = self.name().eql?(other.name())
-    same_class.&(same_name)
+  def self.all
+    returned_tasks = DB.exec("SELECT * FROM tasks;")
+    tasks = []
+    returned_tasks.each() do |task|
+      description = task.fetch("description")
+      due_date = task.fetch("due_date")
+      list_id = task.fetch("list_id").to_i() # The information comes out of the database as a string.
+      tasks.push(Task.new({:description => description, :due_date => due_date, :list_id => list_id}))
+    end
+    tasks
+  end
+
+  def save
+    DB.exec("INSERT INTO tasks (description, due_date, list_id) VALUES ('#{@description}', '#{@due_date}', #{@list_id});")
+  end
+
+  def ==(another_task)
+    self.description().==(another_task.description()).&(self.list_id().==(another_task.list_id())).&(self.due_date().==(another_task.due_date()))
   end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,22 +1,50 @@
 require 'rspec'
 require 'pg'
 require 'task'
+require 'spec_helper'
 # require 'test'
 
-DB = PG.connect({:dbname => 'to_do_test'})
-
-RSpec.configure do |config|
-  config.after(:each) do
-    DB.exec("DELETE FROM tasks *;")
-  end
-end
-
 describe(Task) do
+  describe(".all") do
+    it("is empty at first") do
+      expect(Task.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it("adds a task to the array of saved tasks") do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date =>"2018-03-30 00:00:00"})
+      test_task.save()
+      expect(Task.all()).to(eq([test_task]))
+    end
+  end
+
+  describe("#description") do
+    it("lets you read the description out") do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date =>"2018-03-30"})
+      expect(test_task.description()).to(eq("learn SQL"))
+    end
+  end
+
+  describe("#list_id") do
+    it("lets you read the list ID out") do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date =>"2018-03-30"})
+      expect(test_task.list_id()).to(eq(1))
+    end
+  end
+
   describe("#==") do
-    it("is the same task if it has the same description") do
-      task1 = Task.new({:description => "learn SQL"})
-      task2 = Task.new({:description => "learn SQL"})
+    it("is the same task if it has the same description and list ID") do
+      task1 = Task.new({:description => "learn SQL", :list_id => 1, :due_date =>"2018-03-30"})
+      task2 = Task.new({:description => "learn SQL", :list_id => 1, :due_date =>"2018-03-30"})
       expect(task1).to(eq(task2))
+    end
+  end
+
+  describe("#due_date") do
+    it("returns the due date") do
+      task1 = Task.new({:description => "learn SQL", :list_id => 1, :due_date =>"2018-03-30"})
+      expect(task1.due_date()).to(eq("2018-03-30"))
     end
   end
 end
